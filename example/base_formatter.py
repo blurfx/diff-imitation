ecb891bd36c7d26e6fa2b0b6c5af541274aa7626
changhui lee <iam@xo.dev>
2020-07-16 22:50:13 +0900

    feat: add abstract base formatter class

---
+++ example/base_formatter.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from diff import DiffResult
+
+
+class BaseFormatter(ABC):
+    @abstractmethod
+    def __init__(self, items: List[DiffResult]):
+        pass
+
+    @abstractmethod
+    def _get_range(self) -> str:
+        pass
+
+    @abstractmethod
+    def format(self) -> str:
+        pass
