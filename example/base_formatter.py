51e107cc5d61d6eab4560005b707833a3ca00ab3
changhui lee <iam@xo.dev>
2020-07-16 23:53:35 +0900

    doc: add comments on demo application

--- example/base_formatter.py
+++ example/base_formatter.py
@@ -4,8 +4,17 @@ from abc import ABC, abstractmethod
 class BaseFormatter(ABC):
     @abstractmethod
     def get_range(self) -> str:
+        """
+        get changed range of document as ed style format (e.g. "11,15d16", "0a1,6")
+
+        :return: colored changed range of document
+        """
         pass
 
     @abstractmethod
     def format(self) -> str:
+        """
+        get colored and formatted diff output string (separated by '\n')
+        :return: colored and formatted diff output
+        """
         pass
