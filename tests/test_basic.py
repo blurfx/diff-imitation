2dd4cc98f4be93a5c405b7a49a6c7f09e873a824
changhui lee <iam@xo.dev>
2020-07-16 18:35:06 +0900

    chore: add more basic test cases

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -23,3 +23,35 @@ class BasicTest(unittest.TestCase):
         actual_result = diff(original_list, modified_list)
 
         assert expected_result == actual_result
+
+    def test_changed_head_and_tail(self):
+        original_list = ["line 1", "line 2"]
+        head_modified_list = ["line1", "line 2"]
+        tail_modified_list = ["line 1", "line2"]
+
+        expected_result = [
+            (-1, "line 1"),
+            (1, "line1"),
+        ]
+        actual_result = diff(original_list, head_modified_list)
+        assert expected_result == actual_result
+
+        expected_result = [
+            (-2, "line 2"),
+            (2, "line2"),
+        ]
+        actual_result = diff(original_list, tail_modified_list)
+        assert expected_result == actual_result
+
+    def test_deleted_head_and_tail(self):
+        original_list = ["line 1", "line 2"]
+        head_modified_list = ["line 2"]
+        tail_modified_list = ["line 1"]
+
+        expected_result = [(-1, "line 1")]
+        actual_result = diff(original_list, head_modified_list)
+        assert expected_result == actual_result
+
+        expected_result = [(-2, "line 2")]
+        actual_result = diff(original_list, tail_modified_list)
+        assert expected_result == actual_result
